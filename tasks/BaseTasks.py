227a6ca6159b887bc7607f0068948b41fe51a098
Limuran <strelgl2@gmail.com>
2023-09-19 17:31:10 +1000

    Изменение структуры методов анализа

--- tasks/BaseTasks.py
+++ tasks/BaseTasks.py
@@ -1,45 +1,30 @@
-import os
 from abc import ABC, abstractmethod
-import vars
 import pandas as pd
-from utils import utils
 
 
 class BaseTask(ABC):
     task_name: str
-    root_dir: str
-    obj_name: str
+    result: pd.DataFrame
 
-    excel_dir_path: str
-    graphs_dir_path: str
+    def run(self) -> None:
+        self.result = self.calculate_data()
 
-    def __init__(self):
-        self.root_dir = os.path.join(vars.RESULTS_DIR, self.task_name)
-        self.excel_dir_path = os.path.join(self.root_dir, "Excel таблицы")
-        utils.create_dir(self.excel_dir_path)
-        self.graphs_dir_path = os.path.join(self.root_dir, "Графики")
-        utils.create_dir(self.graphs_dir_path)
-
-    def run(self, draw_graphs: bool = False):
-        data = self.calculate_data()
-        if draw_graphs:
-            self.save_graphs(data)
-        self.save_excel(data)
+    def get_data(self) -> pd.DataFrame:
+        if not hasattr(self, "result"):
+            self.run()
+        return self.result
 
     @abstractmethod
-    def calculate_data(self):
+    def calculate_data(self) -> pd.DataFrame:
         pass
 
-    def save_excel(self, data):
-        pass
+    def accept(self, visitor):
+        visitor.visit(self)
 
-    def save_graphs(self, data):
-        pass
+    def save_to_excel(self):
+        from tasks.ExcelVisitor import ExcelVisitor
+        self.accept(ExcelVisitor())
 
-    def get_excel_path(self, file_name: str):
-        return os.path.join(self.excel_dir_path, file_name + ".xlsx")
-
-    def get_graph_img_path(self, file_name: str):
-        return os.path.join(self.graphs_dir_path, file_name + ".png")
-
-
+    def save_to_graphs(self):
+        from tasks.GraphsVisitor import GraphsVisitor
+        self.accept(GraphsVisitor())
