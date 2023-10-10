94f66713221dc8c1176b47c5125b09ee57b2cd08
Limuran <strelgl2@gmail.com>
2023-10-10 14:53:38 +1000

    Настройка xlim и размера точек для графиков

--- utils/save_data_utils.py
+++ utils/save_data_utils.py
@@ -19,23 +19,27 @@ def save_excel_dataframe(path: str, sheets: list[tuple[str, pd.DataFrame]], inde
 
 
 def create_and_save_figure(fig_path: str, y_rows: list[list[int | float]], x_rows: list[list[int | float]] = None,
-                           title: str = "", xlabel: str = "", ylabel: str = "", lim: tuple[float, float] = None,
-                           legend: list = None, legend_title: str = "", fmt_list: list[str] = None, text: str = ""):
+                           title: str = "", xlabel: str = "", ylabel: str = "",
+                           ylim: tuple[float, float] = None, xlim: tuple[float, float] = None,
+                           legend: list = None, legend_title: str = "", fmt_list: list[str] = None, text: str = "",
+                           markersize: float = 3):
     fig, ax = plt.subplots()
     ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    if lim:
-        ax.set_ylim(lim)
+    if ylim:
+        ax.set_ylim(ylim)
+    if xlim:
+        ax.set_xlim(xlim)
     if fmt_list is None:
         fmt_list = ["-"] * len(y_rows)
     if x_rows is None:
         for y, fmt in zip(y_rows, fmt_list):
             x = list(range(len(y)))
-            ax.plot(x, y, fmt)
+            ax.plot(x, y, fmt, markersize=markersize)
     else:
         for x, y, fmt in zip(x_rows, y_rows, fmt_list):
-            ax.plot(x, y, fmt)
+            ax.plot(x, y, fmt, markersize=markersize)
     if legend:
         ax.legend(legend, title=legend_title, loc="upper center", ncol=5, bbox_to_anchor=(0.5, 1))
 
@@ -43,10 +47,10 @@ def create_and_save_figure(fig_path: str, y_rows: list[list[int | float]], x_row
     font = ImageFont.truetype("arial.ttf", size=16)
     fig.canvas.draw()
     fig_img = Image.frombytes('RGB', fig.canvas.get_width_height(), fig.canvas.tostring_rgb())
-    text_space = 200
+    text_space = 250
     fig_img = ImageOps.expand(fig_img, border=(0, 0, text_space, 0), fill=(255, 255, 255))
     drawer = ImageDraw.Draw(fig_img)
-    drawer.text((590, 100), text, fill=0, font=font)
+    drawer.text((590, 20), text, fill=0, font=font)
 
     fig_img.save(fig_path)
     plt.close(fig)
